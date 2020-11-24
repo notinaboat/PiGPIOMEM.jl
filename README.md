973ea2ba2cf189157aa25c958b93914bd522eb1c
notinaboat <notinaboat@icloud.com>
2020-11-24 22:22:27 +1100

    doc updates

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Julia interface for [`/dev/gpiomem'](https://github.com/raspberrypi/linux/blob/r
 `/dev/gpiomem` provides access to the BCM2835 GPIO registers at `0x7E200000`
 (See [BCM2835-ARM-Peripherals.pdf](https://www.raspberrypi.org/app/uploads/2012/02/BCM2835-ARM-Peripherals.pdf) p89.)
 
-Simple examples:
+
+## Simple examples
 
 ```julia
 using PiGPIOMEM
@@ -29,32 +30,17 @@ x = GPIOPin(8, output=true)
 x[] = 1
 ```
 
-Set pin: `pin[] = x`, `set(pin)`, `clear(pin)`.
 
-Input/output: `set_input_mode(pin)` (default),
-              `set_output_mode(pin)`,
-              `is_input(pin)::Bool`,
-              `is_output(pin)::Bool`.
+## Full Documentation
 
-Pullup/pulldown: `set_highz(pin)` (default),
-                 `set_pulldown(pin)`
-                 `set_pullup(pin)`
-
-Set/Get multiple pins: `PiGPIOMEM.set(::GPIOPin...)`,
-                       `PiGPIOMEM.clear(::GPIOPin...)`,
-                       `PiGPIOMEM.level(::GPIOPin...)`.
-
-
-Full documentation in [`src/PiGPIOMEM.jl`](src/PiGPIOMEM.jl) or:
+See [`src/PiGPIOMEM.jl`](src/PiGPIOMEM.jl) or online help:
 
 
 ```julia
 julia> using PiGPIOMEM
 help?> PiGPIOMEM
-...
 
 ```
-
 
 
 # Alternatives
@@ -63,9 +49,9 @@ help?> PiGPIOMEM
 -- uses the [`pigpiod`](http://abyz.me.uk/rpi/pigpio/pigpiod.html)
 daemon interface.
 
-* [ronisbr/BaremetalPi.jl](https://github.com/ronisbr/BaremetalPi.jl)
--- uses `/dev/gpiomem`, `/dev/spidevX.X`, `/dev/i2c-X` etc.
-
 * [notinaboat/PiGPIOC.jl](https://github.com/notinaboat/PiGPIOC.jl)
 -- `ccall` wrappers for the
 [`pigpio` C Interface](http://abyz.me.uk/rpi/pigpio/cif.html).
+
+* [ronisbr/BaremetalPi.jl](https://github.com/ronisbr/BaremetalPi.jl)
+-- uses `/dev/gpiomem`, `/dev/spidevX.X`, `/dev/i2c-X` etc.
--- src/PiGPIOMEM.jl
+++ src/PiGPIOMEM.jl
@@ -1,11 +1,52 @@
-# References:
-# [1] BCM2835-ARM-Peripherals.pdf
-# https://www.raspberrypi.org/app/uploads/2012/02/BCM2835-ARM-Peripherals.pdf
-# [2] BCM2835 GPIO memory device driver
-# https://github.com/raspberrypi/linux/blob/rpi-5.4.y/
-#                    drivers/char/broadcom/bcm2835-gpiomem.c
+"""
+# module PiGPIOMEM
 
+Julia interface for `/dev/gpiomem` on Raspberry Pi [1].
+`/dev/gpiomem` maps the BCM2835 GPIO registers at `0x7E200000` [2, p89].
 
+This module can:
+ * Query and set GPIO pins.
+ * Configure input/output mode and pullups/pulldowns.
+
+## Interface Summary
+
+Create a `GPIOPin` object:
+ * `pin = GPIOPin(7)`
+
+Query pin state:
+ * `x = pin[]` (`Bool`)
+
+Set pin state:
+ * `pin[] = true` -- logic high
+ * `pin[] = false` -- logic low
+ * `set(pin)` -- logic high
+ * `clear(pin)` -- logic low
+
+Configure input/output:
+ * `set_input_mode(pin)` (default)
+ * `set_output_mode(pin)`
+ * `is_input(pin)::Bool`
+ * `is_output(pin)::Bool`
+
+Configure pullup/pulldown:
+ * `set_highz(pin)` (default)
+ * `set_pulldown(pin)`
+ * `set_pullup(pin)`
+
+Set/Get multiple pins:
+ * `PiGPIOMEM.set(::GPIOPin...)`
+ * `PiGPIOMEM.clear(::GPIOPin...)`
+ * `PiGPIOMEM.level(::GPIOPin...)`
+
+## References
+
+ [1] BCM2835-ARM-Peripherals.pdf
+     https://www.raspberrypi.org/app/uploads/2012/02/BCM2835-ARM-Peripherals.pdf
+
+ [2] BCM2835 GPIO memory device driver
+     https://github.com/raspberrypi/linux/blob/rpi-5.4.y/
+                        drivers/char/broadcom/bcm2835-gpiomem.c
+"""
 module PiGPIOMEM
 
 
@@ -19,63 +60,77 @@ using Mmap
 
 # BCM2835 GPIO Registers.
 
+"Memory map of the BCM2835 GPIO registers (`0x7E200000`)"
 const gpiomem = Ref{Vector{UInt32}}()
+const gpiomem_length = 40                                        # [1, 6.1, p91]
 
+
+"""
+    Register(offset)
+
+Wraps a pointer one of the BCM2835 GPIO registers.
+Provides efficient access to register content using direct load/store
+instructions via the `getindex/setindex!` interface.
+The offset is checked once at construction time to ensure it is in bounds.
+The `/dev/gpiomem` map is loaded on demand.
+"""
 struct Register
     address::Ptr{UInt32}
     function Register(offset)
-        @assert 1 + offset in 1:length(gpiomem[])
+        @assert 1 + offset in 1:gpiomem_length
         if !isassigned(gpiomem)
             gpiomem[] = Mmap.mmap("/dev/gpiomem",
-                                  Vector{UInt32}, 40; # [1, 6.1, p91]
+                                  Vector{UInt32}, gpiomem_length;
                                   grow=false)
         end
         new(pointer(gpiomem[], 1 + offset))
     end
 end
 
-gpfsel(n)   = register(n÷10)   # 0x 7E20 0000 [1, 6.1, p90]
-gpset0()    = register(0x1C÷4) # 0x 7E20 001C [1, 6.1, p90]
-gpclr0()    = register(0x28÷4) # 0x 7E20 0028 [1, 6.1, p90]
-gplev0()    = register(0x34÷4) # 0x 7E20 0034 [1, 6.1, p90]
-gppud()     = register(0x94÷4) # 0x 7E20 0094 [1, 6.1, p91]
-gppudclk0() = register(0x98÷4) # 0x 7E20 0098 [1, 6.1, p91]
-
 Base.getindex(r::Register) = unsafe_load(r.address)
 Base.setindex!(r::Register, v) = unsafe_store!(r.address, v)
 
+gpfsel(n)   = Register(n÷10)                        # 0x 7E20 0000 [1, 6.1, p90]
+gpset0()    = Register(0x1C÷4)                      # 0x 7E20 001C [1, 6.1, p90]
+gpclr0()    = Register(0x28÷4)                      # 0x 7E20 0028 [1, 6.1, p90]
+gplev0()    = Register(0x34÷4)                      # 0x 7E20 0034 [1, 6.1, p90]
+gppud()     = Register(0x94÷4)                      # 0x 7E20 0094 [1, 6.1, p91]
+gppudclk0() = Register(0x98÷4)                      # 0x 7E20 0098 [1, 6.1, p91]
+
 
 """
-    GPIOPin(n)
+    GPIOPin(n; [output=false])
 
 Raspberry Pi (BCM2835) General Purpose Input Output pin `n`.
 
-Query pin: `x = pin[]`.
+See `help?>` [PiGPIOMEM](@ref) for Interface Summary.
 
-Set pin: `pin[] = x`, `set(pin)`, `clear(pin)`.
+### Implementation Notes
 
-Input/output: `set_input_mode(pin)` (default),
-              `set_output_mode(pin)`,
-              `is_input(pin)::Bool`,
-              `is_output(pin)::Bool`.
+The implementation aims to minimise pin access overhead.
+The pin number is bounds checked only once at construction time and
+register addresses and bit masks are pre-computed.
 
-Pullup/pulldown: `set_highz(pin)` (default),
-                 `set_pulldown(pin)`
-                 `set_pullup(pin)`
+The GPIOPin object holds direct pointers to the BCM2835 Registers that
+control a specific pin. Rather than storing the pin number, the GPIOPin object
+stores bit masks for the register bits that control the pin. This allows
+the `set` and `clear` functions to be as simple as possible
+(`p.gpset[] = p.pin_bit` and `p.gpclr[] = p.pin_bit` respectively).
 
-Set/Get multiple pins: `PiGPIOMEM.set(::GPIOPin...)`,
-                       `PiGPIOMEM.clear(::GPIOPin...)`,
-                       `PiGPIOMEM.level(::GPIOPin...)`.
+`sel_bit` is the Input/output mode bit for this pin in the GPFSELn register.
+
+`pin_bit` is the bit that controls this pin in the GPSETn, GPCLRn and GPLEVn
+egisters."
 """
 struct GPIOPin
 
-    sel_bit::UInt32 # [1, Table 6-2, p92]
-    gpfsel::Register
+    sel_bit::UInt32                                       # [1, Table 6-2,  p92]
+    gpfsel::Register                                      # [1,       6.1,  p90]
 
     pin_bit::UInt32
-    gpset::Register # [1, Table 6-9,  p95]
-    gpclr::Register # [1, Table 6-10, p95]
-    gplev::Register # [1, Table 6-12, p96]
+    gpset::Register                                       # [1, Table 6-9,  p95]
+    gpclr::Register                                       # [1, Table 6-10, p95]
+    gplev::Register                                       # [1, Table 6-12, p96]
 
     function GPIOPin(n; output=false)
         @assert n in 0:27
@@ -89,14 +144,14 @@ struct GPIOPin
     end
 end
 
-sel_index(p::GPIOPin) = trailing_zeros(p.selpit)
-pin_index(p::GPIOPin) = trailing_zeros(p.binpit)
+sel_index(p::GPIOPin) = trailing_zeros(p.sel_bit)
+pin_index(p::GPIOPin) = trailing_zeros(p.pin_bit)
 
 
 # Set/Get pin state.
 
 Base.setindex!(p::GPIOPin, v::Bool) = v ? set(p) : clear(p)
-Base.setindex!(p::GPIOPin, v) = setindex(p, !iszero(v))
+Base.setindex!(p::GPIOPin, v) = setindex!(p, !iszero(v))
 Base.getindex(p::GPIOPin) = !iszero(level(p))
 
   set(p::GPIOPin) = p.gpset[] = p.pin_bit                 # [1, Table 6-9,  p95]
@@ -106,7 +161,7 @@ level(p::GPIOPin) = p.gplev[] & p.pin_bit                 # [1, Table 6-12, p96]
 
 # Input/Output mode.                                         [1, Table 6-2, p92]
 
-reset_mode(p::GPIOPin) = p.gpfsel[] &= ~(0b111 << sel_index(p))
+reset_mode(p::GPIOPin) = p.gpfsel[] &= ~(UInt32(0b111) << sel_index(p))
 
 set_input_mode(p::GPIOPin) = p.gpfsel[] &= ~p.sel_bit
 set_output_mode(p::GPIOPin) = p.gpfsel[] |= p.sel_bit
@@ -114,9 +169,9 @@ is_input(p::GPIOPin) = iszero(p.gpfsel[] & p.sel_bit)
 is_output(p::GPIOPin) = !is_input(p::GPIOPin)
 
 
-
 # Operations on multiple pins.
 
+"Combine the `pin_bit`s for multiple `pins` into a single bit mask."
 bits(pins...) = reduce(|, p.pin_bit for p in pins)
 
   set(pins::GPIOPin...) = first(pins).gpset[] = bits(pins)
@@ -130,11 +185,14 @@ set_highz(p::GPIOPin) = set_pud(p, 0)
 set_pulldown(p::GPIOPin) = set_pud(p, 1)
 set_pullup(p::GPIOPin) = set_pud(p, 2)
 
+using LLVM
+spin(n) = for i in 1:n LLVM.Interop.@asmcall("nop") end
+
 function set_pud(p::GPIOPin, mode)
     gppud()[] = mode
-    yeild() # FIXME [1, p101] Wait 150 cycles
+    spin(100)                                        # [1, p101] Wait 150 cycles
     gppudclk0()[] = p.pin_bit
-    yeild() # FIXME [1, p101] Wait 150 cycles
+    spin(100)                                        # [1, p101] Wait 150 cycles
     gppud()[] = 0
     gppudclk()[] = 0
 end
